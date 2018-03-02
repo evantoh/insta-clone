1473572a8c9c006eed651931c5b880d7984df45e
evantoh <evanmwenda@gmail.com>
2018-03-02 16:37:11 +0300

    add settings inorder to store pics

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/evans/python-projects/Django-projects/inta-clone/virtual"
+set -gx VIRTUAL_ENV "/home/evans/python-projects/Django-projects/instagram-clone/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
