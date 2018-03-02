f522bf72fe0090bef0a6989d3eaf36f3a110b55e
evantoh <evanmwenda@gmail.com>
2018-03-02 12:55:49 +0300

    add the login and logout templates and define their urls

--- instagram/insta/views.py
+++ instagram/insta/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('welcome to kkjkrj')
+def search_results(request):
+    
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
