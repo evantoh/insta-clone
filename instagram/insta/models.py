195148be0a4bbb22fa5bf64be0dccf7c7a6cd420
evantoh <evanmwenda@gmail.com>
2018-03-02 15:21:47 +0300

    added the profile modelwith its properties

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+# User = get_user_model()
+
 
 # Create your models here.
+
+# create class profile 
+class profile(models.Model):
+    profile_photo=models.ImageField(upload_to = 'profile/')
+    bio=models.TextField()
+    user=models.ForeignKey(User)
